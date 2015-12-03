64ba6ee82b865798b8e46d49d127490cee43e816
Laurent TOSON <laurentt96@outlook.fr>
2015-12-03 23:08:37 +0100

    Ajout actualites.php et MAJ blank

---
+++ alt-f4/actualites.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>AdminLTE 2 | Actualités</title>
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<!-- Bootstrap 3.3.5 -->
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<!-- Theme style -->
+	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+    folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+    	<!-- Site wrapper -->
+    	<div class="wrapper">
+
+    		<header class="main-header">
+    			<!-- Logo -->
+    			<a href="index2.html" class="logo">
+    				<!-- mini logo for sidebar mini 50x50 pixels -->
+    				<span class="logo-mini"><b>A</b>LT</span>
+    				<!-- logo for regular state and mobile devices -->
+    				<span class="logo-lg"><b>Alt</b> - F4</span>
+    			</a>
+    			<!-- Header Navbar: style can be found in header.less -->
+    			<nav class="navbar navbar-static-top" role="navigation">
+    				<!-- Sidebar toggle button-->
+    				<a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+    					<span class="sr-only">Toggle navigation</span>
+    					<span class="icon-bar"></span>
+    					<span class="icon-bar"></span>
+    					<span class="icon-bar"></span>
+    				</a>
+    				<div class="navbar-custom-menu">
+    					<ul class="nav navbar-nav">
+    						<!-- Messages: style can be found in dropdown.less-->
+
+    						<li class="dropdown messages-menu">
+    							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+    								<i class="fa fa-envelope-o"></i>
+    								<span class="label label-success">4</span>
+    							</a>
+    							<ul class="dropdown-menu">
+    								<li class="header">You have 4 messages</li>
+    								<li>
+    									<!-- inner menu: contains the actual data -->
+    									<ul class="menu">
+    										<li><!-- start message -->
+    											<a href="#">
+    												<div class="pull-left">
+    													<img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+    												</div>
+    												<h4>
+    													Support Team
+    													<small><i class="fa fa-clock-o"></i> 5 mins</small>
+    												</h4>
+    												<p>Why not buy a new awesome theme?</p>
+    											</a>
+    										</li><!-- end message -->
+    									</ul>
+    								</li>
+    								<li class="footer"><a href="#">See All Messages</a></li>
+    							</ul>
+    						</li>
+
+
+    						<!-- User Account: style can be found in dropdown.less -->
+    						<li class="dropdown user user-menu">
+    							<a href="#" class="dropdown-toggle" data-toggle="dropdown">
+    								<span class="hidden-xs">USERNAME</span><!-- /!\ inclu 3 times -->
+    							</a>
+    							<ul class="dropdown-menu">
+    								<!-- User image -->
+    								<li class="user-header">
+    									<img src="dist/img/userLogo.jpg" class="img-circle" alt="User Image">
+    									<p>
+    										USERNAME <!-- /!\ inclu 3 times --> 
+    									</p>
+    								</li>
+
+    								<!-- Menu Footer-->
+    								<li class="user-footer">
+    									<div class="pull-left">
+    										<a href="#" class="btn btn-default btn-flat">Profile</a>
+    									</div>
+    									<div class="pull-right">
+    										<a href="#" class="btn btn-default btn-flat">Sign out</a>
+    									</div>
+    								</li>
+    							</ul>
+    						</li>
+    					</ul>
+    				</div>
+    			</nav>
+    		</header>
+
+    		<!-- =============================================== -->
+
+    		<!-- Left side column. contains the sidebar -->
+    		<aside class="main-sidebar">
+    			<!-- sidebar: style can be found in sidebar.less -->
+    			<section class="sidebar">
+    				<!-- Sidebar user panel -->
+    				<div class="user-panel">
+    					<div class="pull-left image">
+    						<img src="dist/img/userlogo.jpg" class="img-circle" alt="User Image">
+    					</div>
+    					<div class="pull-left info">
+    						<p>USERNAME</p><!-- /!\ inclu 3 times -->
+    						<a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+    					</div>
+    				</div>
+    				<!-- sidebar menu: : style can be found in sidebar.less -->
+    				<ul class="sidebar-menu">
+    					<li class="header">PLAN DU SITE</li>
+
+    					<li><a href="#"><i class="fa fa-globe"></i> <span>Carte des évènements</span></a></li>
+    					<li><a href="#"><i class="fa fa-ambulance"></i> <span>Contacts/Lieux utiles</span></a></li>            
+    					<li class="treeview">
+    						<a href="#">
+    							<i class="fa fa-graduation-cap"></i> <span>S'informer pour agir</span>
+    							<i class="fa fa-angle-left pull-right"></i>
+    						</a>
+    						<ul class="treeview-menu">
+    							<li><a href="#"><i class="fa fa-plus-square"></i>Gestes <br>premiers secours</a></li>
+    							<li><a href="#"><i class="fa fa-lightbulb-o"></i>S'informer sur <br>une catastrophe</a></li>
+    						</ul>
+    					</li>
+    					<li class="header">ACTIONS</li>
+    					<li><a href="#"><i class="fa fa-plus"></i> <span>Ajouter une crise</span></a></li>
+    					<li><a href="#"><i class="fa fa-plus"></i> <span>Associer chef secours à <br>une crise</span></a></li>
+    					<li><a href="#"><i class="fa fa-plus"></i> <span>Associer secours à <br>une opération</span></a></li>
+    					<li><a href="#"><i class="fa fa-plus"></i> <span>Ajouter une opération</span></a></li>
+
+    					<li><a href="#"><i class="fa fa-binoculars"></i> <span>Voir mes opérations</span></a></li>
+
+    				</ul>
+    			</section>
+    			<!-- /.sidebar -->
+    		</aside>
+
+    		<!-- =============================================== -->
+
+    		<!-- Content Wrapper. Contains page content -->
+    		<div class="content-wrapper">
+
+    			<!-- Main content -->
+    			<section class="content">
+
+    				<!-- Default box -->
+    				<div class="box">
+    					<div class="col-md-12">
+    						<!-- Custom Tabs -->
+    						<div class="nav-tabs-custom">
+    							<ul class="nav nav-tabs">
+    								<li class="active"><a href="#tab_1" data-toggle="tab">#MGSU</a></li>
+    								<li><a href="#tab_2" data-toggle="tab">#Urgences</a></li>
+    								<li><a href="#tab_3" data-toggle="tab">#Gestion de crise</a></li>
+    								<li><a href="#tab_4" data-toggle="tab">#Médias sociaux</a></li>
+    								<li><a href="#tab_5" data-toggle="tab">#CrowdHelp</a></li>
+    								<li><a href="#tab_6" data-toggle="tab">#Resilience</a></li>
+    							</ul>
+    							<div class="tab-content">
+    								<div class="tab-pane active" id="tab_1">
+    									<a class="twitter-timeline"  href="https://twitter.com/hashtag/MSGU" data-widget-id="672516645894676480">Tweets sur #MSGU</a>
+    									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    								</div><!-- /.tab-pane -->
+    								<div class="tab-pane" id="tab_2">
+    									<a class="twitter-timeline"  href="https://twitter.com/hashtag/Urgences" data-widget-id="672525258637877250">Tweets sur #Urgences</a>
+    									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    								</div><!-- /.tab-pane -->
+    								<div class="tab-pane" id="tab_3">
+    									<a class="twitter-timeline"  href="https://twitter.com/search?q=%23Gestion%20de%20crise" data-widget-id="672531602396856321">Tweets sur #Gestion de crise</a>
+    									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    								</div><!-- /.tab-pane -->
+    								<div class="tab-pane" id="tab_4">
+    									<a class="twitter-timeline"  href="https://twitter.com/search?q=%23M%C3%A9dias%20sociaux" data-widget-id="672532010498396162">Tweets sur #Médias sociaux</a>
+    									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    								</div><!-- /.tab-pane -->
+    								<div class="tab-pane" id="tab_5">
+    									<a class="twitter-timeline"  href="https://twitter.com/hashtag/CrowdHelp" data-widget-id="672532491555741696">Tweets sur #CrowdHelp</a>
+    									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    								</div><!-- /.tab-pane -->
+    								<div class="tab-pane" id="tab_6">
+    									<a class="twitter-timeline"  href="https://twitter.com/hashtag/Resilience" data-widget-id="672534301016236032">Tweets sur #Resilience</a>
+    									<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    								</div><!-- /.tab-pane -->
+    							</div><!-- /.tab-content -->
+    						</div><!-- nav-tabs-custom -->
+    					</div><!-- /.col -->
+    				</div><!-- /.box -->
+
+    			</section><!-- /.content -->
+    		</div><!-- /.content-wrapper -->
+    		<footer class="main-footer">
+    			FOOTER
+    		</footer>
+      <!-- Add the sidebar's background. This div must be placed
+      immediately after the control sidebar -->
+      <div class="control-sidebar-bg"></div>
+  </div><!-- ./wrapper -->
+
+  <!-- jQuery 2.1.4 -->
+  <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
+  <!-- Bootstrap 3.3.5 -->
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <!-- SlimScroll -->
+  <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+  <!-- FastClick -->
+  <script src="plugins/fastclick/fastclick.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/app.min.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="dist/js/demo.js"></script>
+</body>
+</html>
