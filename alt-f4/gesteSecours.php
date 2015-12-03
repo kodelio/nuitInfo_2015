3c118593bd623b23065a0abfbd75e137e54156b1
Adrien Magnin <ma410183@etu.unice.fr>
2015-12-03 21:27:57 +0100

    Ajout gesteSecours.php

---
+++ alt-f4/gesteSecours.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Premiers Gestes</title>
+</head>
+	<div class="content-wrapper">
+		<section class="content-header">
+			<h1>Premiers Gestes</h1>
+		</section>
+		
+		<section>
+			<div class="row">
+			
+			</div>
+		</section>
+	
+<body>
+
+</body>
+
+</html>
